9bd0e0ab883f605e3b231b1852b4258598dcd5cd
unknown <geniusraag@gmail.com>
2022-03-29 00:58:39 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDF9lkWL2cNGtJUjS19D7cVq9zCviYGi2A",
+    authDomain: "telemedicine-6f72a.firebaseapp.com",
+    databaseURL: "https://telemedicine-6f72a-default-rtdb.firebaseio.com",
+    projectId: "telemedicine-6f72a",
+    storageBucket: "telemedicine-6f72a.appspot.com",
+    messagingSenderId: "832957292357",
+    appId: "1:832957292357:web:65b08d715f42ac48ab282c",
+    measurementId: "G-JR41X644BL",
+  agora: {
+    appId: '803773dfd3bc49f6919b065aaf4cc89c'
+  }
+  }
+
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,32 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDF9lkWL2cNGtJUjS19D7cVq9zCviYGi2A",
+    authDomain: "telemedicine-6f72a.firebaseapp.com",
+    databaseURL: "https://telemedicine-6f72a-default-rtdb.firebaseio.com",
+    projectId: "telemedicine-6f72a",
+    storageBucket: "telemedicine-6f72a.appspot.com",
+    messagingSenderId: "832957292357",
+    appId: "1:832957292357:web:65b08d715f42ac48ab282c",
+    measurementId: "G-JR41X644BL",
+  agora: {
+    appId: '803773dfd3bc49f6919b065aaf4cc89c'
+  }
+  }
+  
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
